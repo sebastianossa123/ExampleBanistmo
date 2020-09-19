7cc37b9a93433ffe7b2d7e4d62bf4ea02a7a7b2c
SEBASTI�N OSSA QUINTERO <sebastianossa245469@correo.itm.edu.co>
2020-09-18 20:36:56 -0500

    Versionando

---
+++ src/test/java/co/com/banistmo/stepdefinitions/BanistmoStepDefinitions.java
@@ -0,0 +1,37 @@
+package co.com.banistmo.stepdefinitions;
+import cucumber.api.PendingException;
+import cucumber.api.java.Before;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
+import cucumber.api.junit.Cucumber;
+import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
+import net.serenitybdd.screenplay.actors.OnlineCast;
+
+import static co.com.banistmo.util.WebDriver.chrome;
+import static net.serenitybdd.screenplay.actors.OnStage.*;
+
+public class BanistmoStepDefinitions {
+
+    @Before
+    public void init() {
+        setTheStage(OnlineCast.ofStandardActors());
+    }
+
+    @Given("^the user need to enter the tucumenpanama page$")
+    public void the_user_need_to_enter_the_tucumenpanama_page() throws Throwable {
+        theActorCalled("user");
+        theActorInTheSpotlight().can(BrowseTheWeb.with((chrome())));
+    }
+
+    @When("^the user search the infomation travel$")
+    public void the_user_search_the_infomation_travel() throws Throwable {
+
+    }
+
+    @Then("^the user should see the file pdf correctly$")
+    public void the_user_should_see_the_file_pdf_correctly() throws Throwable {
+
+    }
+
+}
